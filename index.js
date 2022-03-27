3ed3aac43c0af1ece376d01f135a9c2106b51de0
Emily Joy DeHaan <emilydehaan001@gmail.com>
2022-03-27 15:25:25 -0400

    Passed all tests!

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = 'bob'.toUpperCase()
+}
+
+
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+}
+
+
+function overwriteBestCustomer() {
+    bestCustomer = 'maybe bob'
+}
+
+
+const leastFavoriteCustomer = ('Squidward');
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = ("Patrick");
+    return leastFavoriteCustomer
+}
